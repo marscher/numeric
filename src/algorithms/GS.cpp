f261bcfc5589ffd4af455d484947b9f1ca588af4
martin <martin@83c1d299-cda1-4f28-bd0d-5cb887c274ba>
2009-06-03 09:09:44 +0000

    git-svn-id: http://opensvn.csie.org/MUS@18 83c1d299-cda1-4f28-bd0d-5cb887c274ba

---
+++ src/algorithms/GS.cpp
@@ -0,0 +1,24 @@
+/**
+ * \class GS
+ *
+ * \ingroup algorithms
+ *
+ * \brief TODO describe me
+ *
+ * TODO detailed describtion
+ * $Version: $
+ *
+ * $Id:  $
+ * first created on 11:13:53 2009
+ */
+
+#include "GS.h"
+
+GS::GS() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GS::~GS() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/algorithms/GS.h
@@ -0,0 +1,27 @@
+/**
+ * \class GS
+ *
+ * \ingroup algorithms
+ *
+ * \brief TODO describe me
+ *
+ * TODO detailed describtion
+ * $Version: $
+ *
+ * $Id:  $
+ * first created on 11:13:53 2009
+ */
+
+#ifndef GS_H_
+#define GS_H_
+
+/**
+ *
+ */
+class GS {
+public:
+	GS();
+	virtual ~GS();
+};
+
+#endif /* GS_H_ */
