a2c563baab9511fa299c5968430f90de49e7273f
martin <martin@83c1d299-cda1-4f28-bd0d-5cb887c274ba>
2009-06-09 13:03:45 +0000

    git-svn-id: http://opensvn.csie.org/MUS@29 83c1d299-cda1-4f28-bd0d-5cb887c274ba

---
+++ src/algorithms/test.h
@@ -0,0 +1,27 @@
+/**
+ * \class test
+ *
+ * \ingroup /MUS/src/algorithms/test.h
+ *
+ * \brief TODO describe me
+ *
+ * TODO detailed describtion
+ * $Version: $
+ *
+ * $Id:  $
+ * first created on 15:09:23 2009
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+/**
+ *
+ */
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
