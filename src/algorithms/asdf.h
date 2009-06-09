6a49bec5e0f5333026d49e6f228a9d8fbd0c2eb6
martin <martin@83c1d299-cda1-4f28-bd0d-5cb887c274ba>
2009-06-09 13:24:51 +0000

    git-svn-id: http://opensvn.csie.org/MUS@30 83c1d299-cda1-4f28-bd0d-5cb887c274ba

---
+++ src/algorithms/asdf.h
@@ -0,0 +1,27 @@
+/**
+ * \class asdf
+ *
+ * \ingroup /MUS/src/algorithms/asdf.h
+ *
+ * \brief TODO describe me
+ *
+ * TODO detailed describtion
+ * $Version: $
+ *
+ * $Id$
+ * first created on 15:29:26 2009
+ */
+
+#ifndef ASDF_H_
+#define ASDF_H_
+
+/**
+ *
+ */
+class asdf {
+public:
+	asdf();
+	virtual ~asdf();
+};
+
+#endif /* ASDF_H_ */
